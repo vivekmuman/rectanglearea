f5a4ff88b29cd92aeadf7fc8cceb92f7b6ba86eb
vivekmuman <vivekmuman@gmail.com>
2018-12-14 19:43:32 +0000

    Add files via upload

---
+++ calculating the area of a rectangle.py
@@ -0,0 +1,8 @@
+# printing the area of a rectangle
+# taking in 2 user inputs
+
+width = int(input("Please enter the width of your rectangle. "))
+length = int(input("Please enter the length of your rectangle. ")) 
+area = width * length
+print("The area of your rectangle is",area)
+input("Press Enter to continue...")
